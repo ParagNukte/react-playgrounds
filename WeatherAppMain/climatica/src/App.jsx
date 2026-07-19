18b90af4eb0a86b1837e144cf92215b3f62b56a4
ParagNukte <paragnuktek@gmail.com>
2026-07-19 21:28:38 +0530

    organised the folder and project structure

---
+++ WeatherAppMain/climatica/src/App.jsx
@@ -0,0 +1,11 @@
+import "@/App.css";
+
+function App() {
+  return (
+    <div className="text-2xl font-bold bg-red-500 text-white p-4">
+      How is the weather today?
+    </div>
+  );
+}
+
+export default App;
